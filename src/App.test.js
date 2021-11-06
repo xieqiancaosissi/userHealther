a3462fc2508100537e46953f6544ac5d00cc4a23
nature.xie <786281870@qq.com>
2021-11-07 00:33:44 +0800

    feature-3

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 console.log('1111');
+console.log('3333');
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
